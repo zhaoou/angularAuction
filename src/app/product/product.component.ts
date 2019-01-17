3e31440ac634f1622568a9c6c11ec1a7f4e9641e
zhaoou <zhaoouzhao@aliyun.com>
2019-01-16 21:23:43 -0600

    work on product

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  private products: Array<Product>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.products = [
+      new Product(1, 'first product', 1.99, 3.5, 'this is the first product', ['Electrical Product', 'Hardware Device']),
+      new Product(2, 'first product', 1.99, 3.5, 'this is the first product', ['Electrical Product', 'Hardware Device']),
+      new Product(3, 'first product', 1.99, 3.5, 'this is the first product', ['Electrical Product', 'Hardware Device']),
+    ];
+  }
+}
+
+export class Product {
+  constructor(
+    public id: number,
+    public title: string,
+    public price: number,
+    public rating: number,
+    public desc: string,
+    public categories: Array<string>
+  ) {
+  }
 }
